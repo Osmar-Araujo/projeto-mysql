a57752014ece4c098a97a8772de79bc368347214
Osmar-Araujo <araujooa@gmail.com>
2021-12-10 17:40:55 -0300

    Testes de retorno MySQL

--- api-backend/rotas/produtos/index.js
+++ api-backend/rotas/produtos/index.js
@@ -1,7 +1,12 @@
+const TabelaProdutos = require('./TabelaProdutos');
 const roteador = require ('express').Router();
 
-roteador.use('/', (req, res) => {
-    res.send('Ok');
-})
+
+roteador.use('/', async(req, res) => {
+    const products = await TabelaProdutos.listar;
+    res.send(
+        JSON.parse(products)
+    );
+});
 
 module.exports = roteador;
